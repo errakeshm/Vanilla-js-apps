efba4c5b1e0189fa01bca6bbf6501d6017f85b6b
errakeshm <compsc.rakeshmohanty@gmail.com>
2019-04-07 13:34:37 +0530

    committing for backup

--- sliding-puzzle/sliding-puzzle.js
+++ sliding-puzzle/sliding-puzzle.js
@@ -33,6 +33,14 @@
             this.entries[this.currentHolePosition]=this.entries[this.problemSize];
             this.entries[this.problemSize]=temp;
         }
+        checkIfSet(){
+            for(let i=0;i<this.problemSize*this.problemSize;i++){
+                if(i!=this.entries[i]-1){
+                    return false;
+                }
+            }
+            return true;
+        }
     }
     document.getElementById("clickme").addEventListener('click',function(event){
         
@@ -61,14 +69,26 @@
             let div = document.createElement('div');
             div.style.height=div.style.width=`${problem.eachElementSize}px`;
             div.style.transform=`translate(${xPos*problem.eachElementSize}px,${yPos*problem.eachElementSize}px)`;
+            
             if(problem.currentHolePosition!=i){
                 div.classList.add('element');
-                div.style.lineHeight=`${problem.eachElementSize-5}px`
+                div.setAttribute('position',i);
+                div.style.lineHeight=`${problem.eachElementSize-5}px`;
                 div.innerHTML = problem.entries[i];
                 puzzleDiv.append(div);
-            }
-            
-        }
 
+                //Attach events
+                div.addEventListener('click',function(event){
+                    let targetDiv = event.target;
+                    let currPosition = targetDiv.getAttribute('position');
+                    if(currPosition-1==problem.setHole){
+                        
+                    }
+                    if(!problem.checkIfSet()){
+
+                    }
+                });
+            }   
+        }
     });
 })();
